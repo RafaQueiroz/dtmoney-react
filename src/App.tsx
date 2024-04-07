67d963e01144f37784542eac29e69238e5a8567d
Rafael Queiroz <r.queiroz@hotmail.com.br>
2024-04-07 09:24:18 -0300

    add modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
 import { Dashboard } from "./components/Dashboard";
+import Modal from "react-modal";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global"
+import { useState } from "react";
 
 export function App() {
+
+  const [isNewTransactionOpen, setIsNewTransactionOpen] = useState(false);
+
+    function handleNewTransactionOpen(){
+        setIsNewTransactionOpen(true);
+    }
+
+    function handleNewTransactionClose(){
+        setIsNewTransactionOpen(false);
+    }
+
   return (
     <>
-      <Header/>
+      <Header onNewTransactionModalOpen={handleNewTransactionOpen}/>
       <Dashboard />
+
+      <Modal 
+                isOpen={isNewTransactionOpen}
+                onRequestClose={handleNewTransactionClose}
+            >
+                Nova transação 
+            </Modal>
       <GlobalStyle />
     </>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,16 @@
 import { Container, Content } from "./style";
 import logoImg from "../../assets/logo.svg"
 
+interface HeaderProps {
+    onNewTransactionModalOpen: () => void
+}
 
-export function Header(){
+export function Header({onNewTransactionModalOpen}: HeaderProps){
     return(
         <Container>
             <Content>
                 <img src={logoImg} alt="logo" />
-                <button>Nova transação</button>
+                <button onClick={onNewTransactionModalOpen}>Nova transação</button>
             </Content>
         </Container>
     )
